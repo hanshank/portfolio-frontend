5cd98d2481c27f6f53d3f6882c4f132639de11e1
hanshank <hans@hanken.org>
2018-12-03 18:38:29 -0800

    real project text

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -28,41 +28,38 @@ class Portfolio extends Component {
           <div className='project'>
             <div className='project-inner'>
               <h1 className='title'>Portfoliopage</h1>
-              <p className='text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh praesent tristique magna sit amet purus gravida. Mauris cursus mattis molestie a iaculis at erat pellentesque. Eu sem integer vitae justo. Placerat orci nulla pellentesque dignissim enim sit amet venenatis. Commodo sed egestas egestas fringilla phasellus. Pharetra vel turpis nunc eget lorem dolor sed viverra ipsum.</p>
-              <p className='text'><strong>Skills used: </strong>Ruby on Rails, JavaScript, HTML, CSS, Bootstrap</p>
+              <p className='text'>My personal web development portfolio page. The site is running on React.js and is using react router.</p>
+              <p className='text'><strong>Skills used: </strong>React.js, JavaScript, HTML, CSS, SASS</p>
               <a className='button' href="https://hanshanken.com" target="_blank">See Website</a>
             </div>
           </div>
 
           <div className='project'>
             <div className='project-inner'>
-              <h1 className='title'>Muli Clothing</h1>
-              <p className='text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh praesent tristique magna sit amet purus gravida. Mauris cursus mattis molestie a iaculis at erat pellentesque. Eu sem integer vitae justo. Placerat orci nulla pellentesque dignissim enim sit amet venenatis. Commodo sed egestas egestas fringilla phasellus. Pharetra vel turpis nunc eget lorem dolor sed viverra ipsum.</p>
-              <p className='text'><strong>Skills used: </strong>Ruby on Rails, JavaScript, HTML, CSS, Bootstrap</p>
-              <a className='button' href="https://muliclothing.com" target="_blank">See Website</a>
-            </div>
-          </div>
-
-          <div className='project'>
-            <div className='project-inner'>
-              <h1 className='title'>Ulsbygg</h1>
-              <p className='text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh praesent tristique magna sit amet purus gravida. Mauris cursus mattis molestie a iaculis at erat pellentesque. Eu sem integer vitae justo. Placerat orci nulla pellentesque dignissim enim sit amet venenatis. Commodo sed egestas egestas fringilla phasellus. Pharetra vel turpis nunc eget lorem dolor sed viverra ipsum.</p>
-              <p className='text'><strong>Skills used: </strong>Ruby on Rails, JavaScript, HTML, CSS, Bootstrap</p>
+              <h1 className='title'>Ulsbygg - Norwegian contractor</h1>
+              <p className='text'>The customer was extremely happy with their new website. The page is built on an open source CMS for Ruby on Rails called 'Spina CMS'. The page was designed in Sketch and deployed with a custom Digital Ocean server. A lot of vanilla JS code was used to generate features such as image sliders and galleries.</p>
+              <p className='text'><strong>Skills used: </strong>Ruby on Rails, JavaScript, AWS S3, HTML, CSS, SASS, Bootstrap, Sketch, Google Analytics</p>
               <a className='button' href="https://ulsbygg.no" target="_blank">See Website</a>
             </div>
           </div>
 
           <div className='project'>
             <div className='project-inner'>
-              <h1 className='title'>Byes</h1>
-              <p className='text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh praesent tristique magna sit amet purus gravida. Mauris cursus mattis molestie a iaculis at erat pellentesque. Eu sem integer vitae justo. Placerat orci nulla pellentesque dignissim enim sit amet venenatis. Commodo sed egestas egestas fringilla phasellus. Pharetra vel turpis nunc eget lorem dolor sed viverra ipsum.</p>
-              <p className='text'><strong>Skills used: </strong>Ruby on Rails, JavaScript, HTML, CSS, Bootstrap</p>
-              <a className='button' href="https://byes.no" target="_blank">See Website</a>
+              <h1 className='title'>Muli Clothing - A fashion startup</h1>
+              <p className='text'>Fashion startup company located in Los Angeles California. The company can now focus on selling their baby products instead of worrying about their website. The site is powered by WordPress and uses WooCommerce as the ecommerce platform. The outstanding photographer <a href='https://tonemolnes.no/' target='_blank'>Tone Molnes</a> made the design really stand out and was a joy to collaborate with on this project.</p>
+              <p className='text'><strong>Skills used: </strong>WordPress, WooCommerce, JavaScript, HTML, CSS, PHP, Google Analytics</p>
+              <a className='button' href="https://muliclothing.com" target="_blank">See Website</a>
             </div>
           </div>
 
-          <Footer />
-
+          <div className='project'>
+            <div className='project-inner'>
+              <h1 className='title'>Byes Funeral Home</h1>
+              <p className='text'>A page I made for a former employer while working there. The aim of the site was to stand out from the local competitors. Now they can! Using Google Analytics has helped them to better discover who their visitors are.</p>
+              <p className='text'><strong>Skills used: </strong>WordPress, HTML, CSS, JavaScript, Google Analytics</p>
+              <a className='button' href="https://byes.no" target="_blank">See Website</a>
+            </div>
+          </div>
         </div>
       </div>
 
