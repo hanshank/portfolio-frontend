ac085f68505cbaaea2fe3d40e1c17aecf03b265e
hanshank <hans@hanken.org>
2018-12-03 18:45:12 -0800

    text changes in about

--- src/components/About.js
+++ src/components/About.js
@@ -13,7 +13,7 @@ class About extends Component {
         <div className='page-content fullscreen-center'>
           <div>
             <h1 className='text-center title'>About Me</h1>
-            <p className='text'>I am a Full Stack Web Developer from Norway, living in Los Angeles, California. consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh praesent tristique magna sit amet purus gravida. Mauris cursus mattis molestie a iaculis at erat pellentesque. Eu sem integer vitae justo. Placerat orci nulla pellentesque dignissim enim sit amet venenatis. Commodo sed egestas egestas fringilla phasellus. Pharetra vel turpis nunc eget lorem dolor sed viverra ipsum.</p>
+            <p className='text'>I am a Full Stack Web Developer from Norway. I am now living in Los Angeles, California with my lovely wife and dog. I have a great passion for creating modern and functional websites with react.js and Ruby on Rails. There is no challenge too large or too small.</p>
             <p className='text'><strong>Top Skills: </strong>Ruby, Ruby on Rails, JavaScript, HTML, CSS, React.js</p>
             <p className='text'><strong>Tools I use: </strong>Sass, Git, GitHub, postgreSQL, mySQL, WordPress</p>
           </div>
